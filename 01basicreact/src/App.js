7f651c1bcd4763a4929f9d4a307d174e6032598e
AdityaJodha26 <singhrathoreaditya04@gmail.com>
2026-08-02 03:36:25 +0000

    commit

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -2,7 +2,7 @@ import Youtube from "./Youtube.jsx";
 
 function App() {
   return (
-   <><h1> classic react App</h1>
+   <><h1> classic react App</h1>.  // this is jsx 
    <Youtube/>
    </>
   );
